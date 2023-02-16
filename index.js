0cad4153b8a782688b8efc01773317df53840bd6
CollinBeczak <ctbeczak08@gmail.com>
2023-02-16 17:56:07 -0600

    reset

--- index.js
+++
@@ -1,9 +0,0 @@
-const hamburgerListClick = () => {
-  let listStatus =  document.getElementById("list")
-
-  if(listStatus.classList == 'openList') {
-    listStatus.classList.remove('openList');
-  } else {
-    listStatus.classList.add('openList');
-  }
-}
